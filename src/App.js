b5b3c113a2a78ea3d9ec3ff9b714d7ee54b43b72
mukeshgithub12 <92305330+mukeshgithub12@users.noreply.github.com>
2022-07-13 11:45:18 +0530

    redux changss

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Demo from './components/Demo';
 function App() {
   return (
     <div className="App">
-      <center><h1> Welcome  To React Js in  One Videos</h1></center>
+      <center><h1> Welcome  To React Js in  One Videos 120</h1></center>
       {/* <Home/>
       <Profile/>
       <FunctionBase title="Function Base (props)" title2={{name:'Mukesh Kumar'}}/>
